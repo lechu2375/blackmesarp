4abea48b1b168be523142bb5d86285870e381782
Adam <adamos2375@gmail.com>
2021-05-10 19:41:05 +0200

    Simple Experience System
    Use character methods GetXp,SetXp,GetLevel

---
+++ plugins/experience/sh_plugin.lua
@@ -0,0 +1,13 @@
+PLUGIN.name = "Experience System"
+PLUGIN.author = "Lechu2375"
+PLUGIN.lvlUpExp = 100 //starting exp
+PLUGIN.lvlExpMultiplier = 1.64 //second level will be above 164 third will be above 268
+ix.char.RegisterVar("xp", { //GetXp SetXp
+	field = "xp",
+	fieldType = ix.type.number,
+	default = 0,
+	isLocal = true,
+	bNoDisplay = true
+})
+
+ix.util.Include("sv_meta.lua")
---
+++ plugins/experience/sv_meta.lua
@@ -0,0 +1,25 @@
+local CHAR = ix.meta.character
+
+function CHAR:AddXP(amount)
+    local XpAmount = self:GetXp()
+    self:SetXp(XpAmount+amount)
+    //maybe notification here??
+end
+
+local Exp = PLUGIN.lvlUpExp
+local ExpMultiplier = PLUGIN.lvlExpMultiplier
+function CHAR:GetLevel()
+    local XpAmount = self:GetXp()
+    local CurrentLevel = 1
+    local AExp = Exp
+
+    while(true)do
+        AExp=AExp*ExpMultiplier
+        if(XpAmount>=AExp) then
+            CurrentLevel=CurrentLevel+1
+        else
+            return CurrentLevel
+        end
+    end
+end
+
